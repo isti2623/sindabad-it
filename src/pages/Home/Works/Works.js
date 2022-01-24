2d2c51a42138cb657628728c65a21bf26ebed810
isti2623 <istiaqueahmed2019.bd@gmail.com>
2022-01-24 11:55:18 +0600

    workd added first carousel

--- src/pages/Home/Works/Works.js
+++ src/pages/Home/Works/Works.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import sliderOne from '../../../images/slider_one.PNG'
+import sliderTwo from '../../../images/slider_two.PNG'
 
 const Works = () => {
     return (
@@ -6,6 +8,81 @@ const Works = () => {
             <div className='text-center mt-28 font-semibold mb-7 '>
                 <h2 className='mb-7 text-5xl'>OUR <span className='text-blue-500'>WORKS</span></h2>
             </div>
+
+
+            <div
+                id="carouselExampleCrossfade"
+                class="carousel slide carousel-fade relative mb-11"
+                data-bs-ride="carousel"
+            >
+                <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
+                    <button
+                        type="button"
+                        data-bs-target="#carouselExampleCrossfade"
+                        data-bs-slide-to="0"
+                        class="active"
+                        aria-current="true"
+                        aria-label="Slide 1"
+                    ></button>
+                    <button
+                        type="button"
+                        data-bs-target="#carouselExampleCrossfade"
+                        data-bs-slide-to="1"
+                        aria-label="Slide 2"
+                    ></button>
+                    <button
+                        type="button"
+                        data-bs-target="#carouselExampleCrossfade"
+                        data-bs-slide-to="2"
+                        aria-label="Slide 3"
+                    ></button>
+                </div>
+                <div class="carousel-inner relative w-full overflow-hidden">
+                    <div class="carousel-item active float-left w-full">
+                        <img
+                            src={sliderOne}
+                            class="block w-full"
+                            alt="Wild Landscape"
+                        />
+                    </div>
+                    <div class="carousel-item float-left w-full">
+                        <img
+                            src={sliderTwo}
+                            class="block w-full"
+                            alt="Camera"
+                        />
+                    </div>
+                    <div class="carousel-item float-left w-full">
+                        <img
+                            src={sliderOne}
+                            class="block w-full"
+                            alt="Exotic Fruits"
+                        />
+                    </div>
+                </div>
+                <button
+                    class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+                    type="button"
+                    data-bs-target="#carouselExampleCrossfade"
+                    data-bs-slide="prev"
+                >
+                    <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button
+                    class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+                    type="button"
+                    data-bs-target="#carouselExampleCrossfade"
+                    data-bs-slide="next"
+                >
+                    <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+
+
+
+
         </div>
     );
 };
