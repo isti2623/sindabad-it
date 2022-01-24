88ea0fa627a923a1f447ff8e87564722aee389bb
isti2623 <istiaqueahmed2019.bd@gmail.com>
2022-01-24 11:20:03 +0600

    added service text

---
+++ src/pages/Home/Service/Service.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Service = () => {
+    return (
+        <div>
+            <div className='text-center mt-28 font-semibold '>
+                <h2 className='text-slate-500 mb-7'>SERVICES WE DELIVER</h2>
+                <h2 className='text-4xl'>Preparing for your success, <br />
+                    we provide <span className='text-blue-500'>truly prominent IT solutions.</span></h2>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
