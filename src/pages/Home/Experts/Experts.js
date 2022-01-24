fed6cf0efd13f1b72d39ea4ba7e595cdbf0b4b5f
isti2623 <istiaqueahmed2019.bd@gmail.com>
2022-01-24 18:02:17 +0600

    added some bug

--- src/pages/Home/Experts/Experts.js
+++ src/pages/Home/Experts/Experts.js
@@ -4,7 +4,7 @@ const Experts = () => {
     return (
         <div className='md:flex container mx-auto mb-10'>
             <div>
-                <h2 className='font-bold text-4xl mt-24 mr-4'>Our <span className='text-blue-500'>experienced</span><br /> experts</h2>
+                <h2 className='font-bold md:text-6xl text-4xl mt-24 mr-4'>Our <span className='text-blue-500'>experienced</span><br /> experts</h2>
                 <h2 className='leading-7 mt-5'>Our team members are key to fulfilling our customers needs. <br /> We don’t hire for a particular assignment – we hire people based on their skills, cultural fit, and career goals. <br /> Our philosophy has always been to find the right person, <br /> then find the right opportunity.</h2>
                 <h2 className='leading-7 mt-10'>We take every possible ownership of yours’s <br /> to build your Web and Mobile Applications that vow <br /> your success and help you achieve your dream.</h2>
                 <button className='bg-blue-500 rounded-md p-2 mt-7  font-semibold md:w-48 text-white mr-3'>Join Us Now</button>
