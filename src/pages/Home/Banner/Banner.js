c8a352cae1e8199338ff4cb0ad47d77ef513fa46
isti2623 <istiaqueahmed2019.bd@gmail.com>
2022-01-24 11:10:16 +0600

    update banner responsive

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -3,18 +3,18 @@ import React from 'react';
 const Banner = () => {
     return (
         <div className='container mx-auto mt-20'>
-            <div className='flex'>
+            <div className='md:flex'>
                 <div>
-                    <h2 className='font-semibold text-7xl tracking-wide'>Top Web and Mobile App Development Company.</h2>
+                    <h2 className='font-semibold md:text-7xl tracking-wide'>Top Web and Mobile App Development Company.</h2>
                     <p className='mt-7'>Accelerating digital innovation for the world</p>
                     <p className='mt-7'>Share your idea, we will build your product and help achieve your vision.</p>
-                    <p className='mt-7 font-semibold text-3xl'>Talk with our experts for 30 <br /> minutes of free product <br />
+                    <p className='mt-7 font-semibold md:text-3xl'>Talk with our experts for 30 <br /> minutes of free product <br />
                         consultation!</p>
                     <button className='bg-blue-500 rounded-md p-2 mt-7 font-semibold text-white'>Get A Free Consultation</button>
                 </div>
                 <div>
                     <img className='w-2/4' src="https://res.cloudinary.com/abdulprogton/image/upload/v1620743309/progton-website/custom-images/progton-5-year-logo.webp" alt="" />
-                    <span className='text-4xl font-semibold'>Celebrating 5 Years of <br /> Success!</span>
+                    <span className='md:text-4xl font-semibold'>Celebrating 5 Years of <br /> Success!</span>
                 </div>
             </div>
         </div>
