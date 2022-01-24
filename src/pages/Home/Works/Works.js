7abc50fa0c6e5e498b9ad95d3916b979cef95bbd
isti2623 <istiaqueahmed2019.bd@gmail.com>
2022-01-24 11:40:45 +0600

    added work text

---
+++ src/pages/Home/Works/Works.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Works = () => {
+    return (
+        <div>
+            <div className='text-center mt-28 font-semibold mb-7 '>
+                <h2 className='mb-7 text-5xl'>OUR <span className='text-blue-500'>WORKS</span></h2>
+            </div>
+        </div>
+    );
+};
+
+export default Works;
